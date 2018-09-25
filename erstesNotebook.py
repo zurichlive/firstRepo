3abd251595450a27b46bd4881cfe6f2c88927cc0
zurichlive <florian@kolabnow.ch>
2018-09-25 10:45:12 +0200

    executable python erstesNotebook

---
+++ erstesNotebook.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+#help
+
+
+# In[ ]:
+
+
+#help()
+
+
+# #Titel
+
+# #Titel
+
+# ###Titel **fett**
+
+# 1. test
+# 2. test
+# 3. test
+
+# In[11]:
+
+
+var1 = False
+
+
+# In[12]:
+
+
+if(var1):
+    print ("world is flat")
+else: 
+    print ("world is not flat")
+    
+
+
+# In[ ]:
+
+
+var2 = input("How old are you?")
+
+
+# In[ ]:
+
+
+print (var2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
